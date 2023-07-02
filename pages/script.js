4b21bbfaab74069a22216817e99303540dbcf091
MkZhdanov <differentsky27@gmail.com>
2023-07-02 16:18:27 +0700

    profileEditButton

---
+++ pages/script.js
@@ -0,0 +1,7 @@
+let profileEditButton = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+
+
+
+
+profileEditButton.addEventListener('click',popup.classList.add('popup_opened'));
